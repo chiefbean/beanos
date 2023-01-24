2eadafa1dae96f1565dd40ca4cfe8e923b814415
chiefbean <stevenkarschnia.com>
2023-01-23 19:53:56 -0600

    delete old and created old branch

--- OLD/util.c
+++
@@ -1,7 +0,0 @@
-//Code in this file is adapted from https://cs.bham.ac.uk/~exr/lectures/opsys/10_11/lectures/os-dev.pdf
-
-void memory_copy(char* source, char* dest, int no_bytes) {
-	for(int i = 0; i < no_bytes; i++) {
-		*(dest + i) = *(source + i);
-	}
-}
--- OLD/util.h
+++
@@ -1,3 +0,0 @@
-//Code in this file is copied from https://cs.bham.ac.uk/~exr/lectures/opsys/10_11/lectures/os-dev.pdf
-
-void memory_copy(char* source, char* dest, int no_bytes);
