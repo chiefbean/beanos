cb30b7b1dd043a201a1508b6b2c041533cf101b1
chiefbean <stevenkarschnia.com>
2024-05-09 15:35:44 -0500

    updates

--- libc/kernel/paging.c
+++ libc/kernel/paging.c
@@ -4,12 +4,12 @@
 extern void loadPageDirectory(uint32_t*);
 extern void enablePaging();
 
-uint64_t page_dir_ptr_tab[4] __attribute__((aligned(0x20)));
-uint64_t page_dir[512] __attribute__((aligned(0x1000)));
+uint32_t page_dir_ptr_tab[4] __attribute__((aligned(0x20)));
+uint32_t page_dir[512] __attribute__((aligned(0x1000)));
 
 void enable_paging() {
 
-	page_dir_ptr_tab[0] = (uint64_t)&page_dir | 1;
+	page_dir_ptr_tab[0] = (uint32_t)&page_dir | 1;
 	page_dir[0] = 0b10000011;
 
 	asm volatile ("movl %%cr4, %%eax; bts $5, %%eax; movl %%eax, %%cr4" ::: "eax"); // set bit5 in CR4 to enable PAE		 
